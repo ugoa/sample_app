baef63f943a9ffe67969383b76c56b1a8019f4a6
David Hu <hoodavy@gmail.com>
2013-09-22 19:42:15 +0800

    11.2.5 Got the 'Follow/Unfollow' start to working, via Ajax(which is so awesome.)

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,14 +2,20 @@ class RelationshipsController < ApplicationController
   before_filter :signed_in_user
 
   def create
-    @idol = User.find(params[:relationship][:followed_id])
-    current_user.follow! @idol
-    redirect_to @idol
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow! @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    @bitch = Relationship.find(params[:id]).followed
-    current_user.unfollow! @bitch
-    redirect_to @bitch
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow! @user
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 end
